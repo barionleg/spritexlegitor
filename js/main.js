7ac95445bf28850dd990925c9697755b0c850291
Wojciech Bociański <wbocianski@future-processing.com>
2015-09-02 17:55:16 +0200

    color picker, width height updates

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
     'use strict';
 
-    pallete.show();
+ //   pallete.draw('body');
     
-    pallete.setColor(0,0);
-    pallete.setColor(1,42);
-    pallete.setColor(2,202);
-    pallete.setColor(3,140);
+//    pallete.colorSet(0,0);
+//    pallete.colorSet(1,42);
+//    pallete.colorSet(2,202);
+//    pallete.colorSet(3,140);
 
-    var mydiv = $("<div></div>").addClass('new_line').addClass('color_2').addClass('usercolor');
+    editor.init();
+    
+    var mydiv = $("<div></div>").addClass('new_line').addClass('color_1').addClass('usercolor');
     $('body').append(mydiv);
 
 });
@@ -18,8 +20,8 @@ Number.prototype.clamp = function(min, max) {
   return Math.min(Math.max(this, min), max);
 };
 
-pallete = {
-    show:function(){
+var pallete = {
+    draw:function(container){
         var i,c,$ndiv;
         for (i=0;i<256;i++){
             c=this.getRGB(i);
@@ -29,7 +31,7 @@ pallete = {
                 .attr('id','col_'+i)
                 .attr('title',i);
             if (i%16==0) $ndiv.addClass('pallete_firstinrow');
-            $('body').append($ndiv);
+            $(container).append($ndiv);
         }
     },
     getRGB:function(cval){
@@ -54,7 +56,108 @@ pallete = {
         
         return "rgb("+rr+","+gg+","+bb+")";
     },
-    setColor:function(cnum,cval) {
-        $(".color_"+cnum).css("background-color",this.getRGB(cval));
+    colorSet:function(cnum,cval) {
+        var rgb = this.getRGB(cval);
+        jss.set('.color_'+cnum, {
+            'background-color':rgb
+        });
     },
+};
+
+var config = {
+    default_width: 1,
+    default_height: 8,
+    max_width: 8,
+    max_height: 48,
+    default_c0: 0,
+    default_c1: 41,
+    default_c2: 202,
+    default_c3: 140,
 }
+
+var editor = {
+    sprite : [],
+    mask : [],
+    width: config.default_width,
+    height: config.default_height,
+    colors: [config.default_c0,config.default_c1,config.default_c2,config.default_c3],
+    color: 0,
+    init: function() {
+        $('.usercolor').bind('click',this.pickerClick);
+        $("#width_dec").bind('click',this.widthDec);
+        $("#width_inc").bind('click',this.widthInc);
+        $("#height_dec").bind('click',this.heightDec);
+        $("#height_inc").bind('click',this.heightInc);
+        this.spriteClear();
+        this.colorsUpdate();
+        this.sizeUpdate();
+    },
+
+    widthDec: function(){                       // ************** SIZES
+        if (editor.width>1) {
+            --editor.width;
+            editor.sizeUpdate();
+        }
+    },
+    widthInc: function(){
+        if (editor.width<config.max_width) {
+            ++editor.width;
+            editor.sizeUpdate();
+        }
+    },
+    heightDec: function(){
+        if (editor.height>1) {
+            --editor.height;
+            editor.sizeUpdate();
+        }
+    },
+    heightInc: function(){
+        if (editor.height<config.max_height) {
+            ++editor.height;
+            editor.sizeUpdate();
+        }
+    },
+    sizeUpdate: function() {
+        $("#size_width").html(this.width);
+        $("#size_height").html(this.height);
+    },
+
+    
+    pickerClick: function() {                   // ************** COLORS
+        editor.colorPick(this.id.substr(-1));
+    },
+    colorPick: function(cnum) {
+        this.color = cnum;
+        this.colorsUpdate();
+    },
+    colorSet:function(cnum,cval) {
+        this.colors[cnum,cval];
+        this.colorsUpdate()
+    },
+    colorsUpdate: function() {
+        this.colors.forEach(function(cval,cnum){
+            pallete.colorSet(cnum,cval);   
+        });
+        $(".usercolor").removeClass('color_picked');
+        $("#usercolor_"+this.color).addClass('color_picked');
+    },
+
+    spriteClear: function() {                   // ************** SPRITES
+        function zeros(dimensions) {
+            var array = [];
+            for (var i = 0; i < dimensions[0]; ++i) {
+                array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+            }
+            return array;
+        }
+        this.sprite = zeros([config.max_width*4,config.max_height]);
+        this.mask = zeros([config.max_width*4,config.max_height]);
+        //console.log(this.sprite);
+    },
+    
+    
+    
+    
+
+
+};
